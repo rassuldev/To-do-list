1705ae3400d55d1bf9a87252bd268d4725052583
rassuldev <rassul.sarsembayevs@gmail.com>
2022-09-03 04:34:22 +0600

    Small updates

--- todolistapp/views.py
+++ todolistapp/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from .models import ToDoListItem
 from django.http import HttpResponseRedirect
+
+
 def todolistView(request):
     all_todo_items = ToDoListItem.objects.all()
     return render(request, 'todolist.html', {'all_items': all_todo_items})
+
+
 def addTodoView(request):
     x = request.POST['content']
     new_item = ToDoListItem(content=x)
     new_item.save()
     return HttpResponseRedirect('/todolistapp/')
+
+
 def deleteTodoView(request, i):
     y = ToDoListItem.objects.get(id=i)
     y.delete()
